a462e85457395ec763c35f51df54e9bebc285c7a
sidneijnr <juniorsalustianopaulino@gmail.com>
2023-03-09 14:25:47 -0400

    projetos antigos adicionados ao repositório

---
+++ exercicios/exercicios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("nome:\t\t Sidnei Salustiano Paulino Junior");
+            Console.WriteLine("data de nasc:\t 23/08/2006");
+            Console.WriteLine("idade: \t\t 16 anos");
+            Console.WriteLine("E-mail:\t\t juniorsalustianopaulino@gmail.com");
+            
+
+            
+            
+                    
+        }
+    }
+}
